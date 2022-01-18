4f62426c6d87b57ade84711704857fd391fc962c
bit-human <lincolnhe2008@gmail.com>
2022-01-18 01:37:34 -0500

    a

--- getters/gettext.js
+++ getters/gettext.js
@@ -10,34 +10,9 @@ export const gettext = async (json, client) => {
 		res.on('data', (chunk) => {
 			response = response + chunk.toString();
 		}).on('end', () => {
-			var data = JSON.parse(response);
-			var title = encodeURIComponent(data.query.pages[Object.keys(data.query.pages)[0]].title);
-			secondCall(title, json, data, client);
-//			json.wt = encodeURIComponent(data.query.pages[Object.keys(data.query.pages)[0]].title);
-//			console.log(`https://en.wikipedia.org/wiki/${json.wt}`);
-//			got(json, client);
-		});
-	}).on('error', (e) => {
-		console.error(`Got error: ${e.message}`);
-	});
-}
-
-function secondCall(title, json, data, client) {
-	request(`/w/api.php?action=parse&page=${title}&format=json`, (data) => {
-		json.wt = title;
-		json.tx = data.parse.text['*'];
-		console.log(`https://en.wikipedia.org/wiki/${json.wt}`);
-		got(json, client);
-	});
-}
-
-function request(path, callback) {
-	http.get({ host: 'en.wikipedia.org', path: path }, function (res) {
-		var response = '';
-		res.on('data', (chunk) => {
-			response = response + chunk.toString();
-		}).on('end', () => {
-			callback(JSON.parse(response));
+			json.wt = encodeURIComponent(data.query.pages[Object.keys(data.query.pages)[0]].title);
+			console.log(`https://en.wikipedia.org/wiki/${json.wt}`);
+			got(json, client);
 		});
 	}).on('error', (e) => {
 		console.error(`Got error: ${e.message}`);
