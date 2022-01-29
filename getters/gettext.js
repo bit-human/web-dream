e7a7e809de6ea6bfa32cf3bf8d9db611c52a0bb1
bit-human <lincolnhe2008@gmail.com>
2022-01-29 17:26:28 -0500

    a

--- getters/gettext.js
+++ getters/gettext.js
@@ -2,6 +2,7 @@ import { got } from '../gogetter.js';
 
 import pkg from 'follow-redirects';
 const http = pkg.http;
+const https = pkg.https;
 
 const unix = (date) => { return date.getTime() / 1000 };
 const start = unix(new Date('2007.03.01'));
@@ -11,22 +12,27 @@ export const gettext = async (json, client) => {
 	getWikipedia(json, client);
 }
 
+const tags = ['?'];
+
 // get tumblr info
 function getTumblr(json, client) {
-	http.get({ host: 'api.tumblr.com', path: `/v2/tagged?tag=text&before=${Math.random() * (unix(new Date()) - start) + start}&limit=1&filter=html&api_key=aCoaPWBdaJhpwqjwQYJ50wW02EgYjwVK2mmc3yBU0Gs8fHGLZ3` }, function (res) {
+	var time = Math.floor(Math.random() * (unix(new Date()) - start)) + start;
+	var tag = tags[Math.floor(Math.random() * tags.length)];
+	https.get({ host: 'api.tumblr.com', path: `/v2/tagged?tag=${tag}&before=${time}&limit=1&api_key=aCoaPWBdaJhpwqjwQYJ50wW02EgYjwVK2mmc3yBU0Gs8fHGLZ3` }, function (res) {
 		var response = '';
 		res.on('data', (chunk) => {
 			response = response + chunk.toString();
 		}).on('end', () => {
 			var data = JSON.parse(response).response[0];
-			console.log(response);
-			//json.tn = data.blog_name;
-			//json.ti = data.id;
-			//json.ts = data.slug;
-			//data.tx = data.body;
-			
-			console.log(`https://${json.tn}.tumblr.com/post/${json.ti}/${json.ts}`);
-			got(json, client);
+			if (data) {
+				json.tn = data.blog_name;
+				json.ti = data.id;
+				
+				console.log(`https://${json.tn}.tumblr.com/post/${json.ti}`);
+				got(json, client);
+			}
+			else if(errors[0].title != 'Limit Exceeded')
+				getTumblr(json, client);
 		});
 	}).on('error', (e) => {
 		console.error(`Got error: ${e.message}`);
